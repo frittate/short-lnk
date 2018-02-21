9fc3087d15c8313fad9da07575914e0591070861
DESKTOP-FPH99T9\Basti <acedia247@gmail.com>
2018-02-21 16:21:57 +0100

    added validation for users

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -17,7 +17,11 @@ export default class Login extends React.Component {
         let password = this.refs.password.value.trim();
 
         Meteor.loginWithPassword({email}, password, (err) => {
-            console.log("login callback", err);
+            if (err){
+                this.setState({error: e.message});
+            } else {
+                this.setState({error: ''});
+            }
         });
     }
 
@@ -28,7 +32,7 @@ export default class Login extends React.Component {
             
                 {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-                <form action="" onSubmit={this.onSubmit.bind(this)}>
+                <form onSubmit={this.onSubmit.bind(this)} noValidate>
                     <input type="email" ref="email" name="email" placeholder="Email"/>
                     <input type="password" ref="password" name="password" placeholder="Password"/>
                     <button>Login</button>
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -16,13 +16,17 @@ export default class Signup extends React.Component {
         let email = this.refs.email.value.trim();
         let password = this.refs.password.value.trim();
 
-        Accounts.createUser({email, password}, (err) => {
-            console.log('Signup callback', err);
-        });
+        if (password.length < 9) {
+            return this.setState({error: 'Password must be more than 8 characters.'});
+        }
 
-/*         this.setState({
-            error: 'Something went wrong.'
-        }) */
+        Accounts.createUser({email, password}, (err) => {
+            if (err) {
+                this.setState({error: err.reason});
+            } else {
+                this.setState({error: ''})
+            }
+        });
     }
 
     render(){
@@ -32,7 +36,7 @@ export default class Signup extends React.Component {
             
             {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-            <form action="" onSubmit={this.onSubmit.bind(this)}>
+            <form onSubmit={this.onSubmit.bind(this)} noValidate>
                 <input type="email" ref="email" name="email" placeholder="Email"/>
                 <input type="password" ref="password" name="password" placeholder="Password"/>
                 <button>Create Account</button>
